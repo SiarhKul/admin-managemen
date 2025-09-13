12822c92a5d6886e21af5b8f2c59df1f092eb715
Siarhei Kulazhanka <Siarhkul@gmail.com>
2025-09-13 22:49:31 +0200

    chore: Error handling4

--- apps/server/src/repositories/userRepository.ts
+++ apps/server/src/repositories/userRepository.ts
@@ -1,23 +1,9 @@
 import { readFile } from 'node:fs/promises';
-import { DatabaseError } from '../dtos/errors';
 
 export class UserRepository {
   static async getAllUsers() {
-    try {
-      const users = await readFile('1apps/server/src/db/users.json', 'utf-8');
+    const users = await readFile('1apps/server/src/db/users.json', 'utf-8');
 
-      return JSON.parse(users);
-    } catch (err) {
-      if (err instanceof Error) {
-        if (err.message.includes('ENOENT')) {
-          throw new DatabaseError('Users database file not found', 500);
-        }
-        throw new DatabaseError(`Failed to read users: ${err.message}`, 500);
-      }
-      throw new DatabaseError(
-        'Unknown error occurred while reading users',
-        500
-      );
-    }
+    return JSON.parse(users);
   }
 }
--- apps/server/src/serverces/userService.ts
+++ apps/server/src/serverces/userService.ts
@@ -1,7 +1,22 @@
 import { UserRepository } from '../repositories/userRepository';
+import { readFile } from 'node:fs/promises';
+import { DatabaseError } from '../dtos/errors/index';
 
 export class UserService {
   static async findAllUsers() {
-    return await UserRepository.getAllUsers();
+    try {
+      return await UserRepository.getAllUsers();
+    } catch (err) {
+      if (err instanceof Error) {
+        if (err.message.includes('ENOENT')) {
+          throw new DatabaseError('Users database file not found', 500);
+        }
+        throw new DatabaseError(`Failed to read users: ${err.message}`, 500);
+      }
+      throw new DatabaseError(
+        'Unknown error occurred while reading users',
+        500
+      );
+    }
   }
 }
