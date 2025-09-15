96f4b2f2974ed0b8de52ee78e59811921898352f
Siarhei Kulazhanka <Siarhkul@gmail.com>
2025-09-15 15:20:33 +0200

    chore: Init client

---
+++ apps/client/src/app/components/UserTable.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const UserTable = (props: any) => <div>Test Component</div>;
+
+export default UserTable;
---
+++ apps/client/src/app/services/userApi.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { ERoles, IUser } from '@admin-management/types';
+
+const api = axios.create({
+  baseURL: '/api',
+});
+
+namespace userApi {
+  export const fetchUsers = async (): Promise<IUser[]> => {
+    const { data } = await api.get<IUser[]>('/users');
+    return data;
+  };
+
+  export const updateUserRole = async (
+    userId: number,
+    role: ERoles
+  ): Promise<IUser> => {
+    const { data } = await api.patch<IUser>(`/users/${userId}/role`, { role });
+    return data;
+  };
+
+  export const getAllRoles = async (): Promise<ERoles[]> => {
+    const { data } = await api.get<ERoles[]>('/roles');
+    return data;
+  };
+}
+
+export default userApi;
