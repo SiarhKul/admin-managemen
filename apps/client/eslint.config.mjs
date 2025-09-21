6c2cfb4d16f7fa1bac257d5b5f557fac7e4cd170
Siarhei Kulazhanka <Siarhkul@gmail.com>
2025-09-21 20:50:10 +0200

    chore: playwrith to project

--- apps/client/eslint.config.mjs
+++ apps/client/eslint.config.mjs
@@ -1,7 +1,9 @@
+import playwright from 'eslint-plugin-playwright';
 import nx from '@nx/eslint-plugin';
 import baseConfig from '../../eslint.config.mjs';
 
 export default [
+  playwright.configs['flat/recommended'],
   ...baseConfig,
   ...nx.configs['flat/react'],
   {
